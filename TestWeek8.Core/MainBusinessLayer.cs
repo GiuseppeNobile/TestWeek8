205af605df6e3879fef756e0093f71f489b8cbb0
GiuseppeNobile <giffo.640@gmail.com>
2021-10-29 09:27:07 +0200

    creazione primi modelli

---
+++ TestWeek8.Core/MainBusinessLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeek8.Core
+{
+    public class MainBusinessLayer
+    {
+
+    }
+}
---
+++ TestWeek8.Core/Models/Piatto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeek8.Core.Models
+{
+    public enum Typology
+    {
+        Primo,
+        Secondo,
+        Contorno,
+        Dolce
+    }
+    public class Piatto
+    {
+        public int IdPiatto { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public Typology Tipologia { get; set; }
+        public decimal Prezzo { get; set; }
+    }
+}
